a3d9169c55da0cd906a8af169bbf9bf4a926ac8c
Logan O'Connor <ocplogan@gmail.com>
2026-04-09 20:38:54 -0400

    update

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const errorHandler = require('./middlewares/error-handler');
 const router = require("./routes");
 
 const app = express();
@@ -10,6 +10,21 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message:
+      statusCode === 500
+        ? "An error occurred on the server"
+        : message,
+  });
+});
+
+app.use(errorHandler);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
